b0b99fedb66e1cc2776b91bcb657b6d83ef87031
Shen x. y <87964621+sxy2011@users.noreply.github.com>
2022-10-15 14:07:22 +0800

    06

--- system/UI_main.py
+++ system/UI_main.py
@@ -1,3 +1,22 @@
+import pygame
+
+
 class game:
     def __init__(self):
-        pass
+        WIDTH, HEIGHT = 800, 500
+        WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    def main(self):
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+        pygame.quit()
+
+        return 1
+
+
+me = game()
+me.main()
