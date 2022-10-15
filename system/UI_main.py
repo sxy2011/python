ba050b569cc182784166e0fe41f61bb5dd810d02
Shen x. y <87964621+sxy2011@users.noreply.github.com>
2022-10-15 14:34:58 +0800

    06

--- system/UI_main.py
+++ system/UI_main.py
@@ -3,8 +3,11 @@ import pygame
 
 class game:
     def __init__(self):
+        global WIN, WHITE
         WIDTH, HEIGHT = 800, 500
+        WHITE = (255, 255, 255)
         WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('sxy system')
 
     def main(self):
         run = True
@@ -12,10 +15,13 @@ class game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-
-        pygame.quit()
+            self.draw_window()
 
         return 1
+
+    def draw_window(self):
+        WIN.fill(WHITE)
+        pygame.display.update()
 
 me = game()
 me.main()
