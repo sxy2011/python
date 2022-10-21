cee467a97ab68c584c55ca6683c920dcb6d3d78c
Shen x. y <87964621+sxy2011@users.noreply.github.com>
2022-10-21 18:12:51 +0800

    06

--- system/UImain.py
+++ system/UImain.py
@@ -10,7 +10,7 @@ win = QWidget()
 # 设置
 win.setWindowTitle('system of sxy')
 win.setWindowIcon(QIcon("D:\pythonStudy\python\system\Assets\图标.jpeg"))
-QToolTip.setFont(QFont(''))
+QToolTip.setFont(QFont('隶书', 15))
 win.setToolTip('sxy system')
 
 # 显示
