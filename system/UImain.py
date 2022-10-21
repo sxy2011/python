11bede0ec65f4466d60475db2ca30a418036cd3e
Shen x. y <87964621+sxy2011@users.noreply.github.com>
2022-10-21 19:19:41 +0800

    06

--- system/UImain.py
+++ system/UImain.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtWidgets import QApplication, QWidget, QToolTip
+from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton, QMessageBox
 
 # 实例化
 app = QApplication(sys.argv)
@@ -12,6 +12,10 @@ win.setWindowTitle('system of sxy')
 win.setWindowIcon(QIcon("D:\\pythonStudy\\python\\system\\Assets\\图标.jpeg"))
 QToolTip.setFont(QFont('隶书', 15))
 win.setToolTip('sxy system')
+win.setGeometry(300, 200, 400, 340)
+exit_ = QPushButton('关闭', win)
+exit_.move(350, 300)
+exit_.show()
 
 # 显示
 win.show()
