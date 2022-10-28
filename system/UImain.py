a1a6038093b1bbb5934022b0442492b4e182ce0c
Shen x. y <87964621+sxy2011@users.noreply.github.com>
2022-10-28 17:03:14 +0800

    06

--- system/UImain.py
+++ system/UImain.py
@@ -1,4 +1,7 @@
 import sys
+import threading
+import time
+import _thread
 
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton, QMessageBox
@@ -12,8 +15,17 @@ win.setWindowTitle('system of sxy')
 win.setWindowIcon(QIcon("D:\\pythonStudy\\python\\system\\Assets\\图标.jpeg"))
 win.setGeometry(300, 200, 400, 340)
 exit_ = QPushButton('关闭', win)
-exit_.move(350, 300)
+exit_.move(300, 300)
 exit_.show()
+
+
+def job():
+    time.sleep(100)
+
+
+_thread.start_new_thread(job, exit_function())
+_thread.start_new_thread(job, ())
+job()
 
 # 显示
 win.show()
