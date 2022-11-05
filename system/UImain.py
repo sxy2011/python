c5a8d5e594f4108a935a4629948b6d9adb558314
Shen x. y <87964621+sxy2011@users.noreply.github.com>
2022-11-05 10:01:57 +0800

    06

--- system/UImain.py
+++ system/UImain.py
@@ -1,34 +1,24 @@
 import sys
-import threading
-import time
-import _thread
-
+import function
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton, QMessageBox
-class UI(QWidget):
-    def __init__(self):
-        super.__init__()
-        self.app = QApplication(sys.argv)
-        self.win = QWidget()
 
-    def sitting(self):
-        self.win.setWindowTitle('system of sxy')
-        self.win.setWindowIcon(QIcon("D:\\pythonStudy\\python\\system\\Assets\\图标.jpeg"))
-        self.win.setGeometry(300, 200, 400, 340)
-        return self.win
+# 实例化
+app = QApplication(sys.argv)
+win = QWidget()
 
-    def sitting_exitButton(self):
-        self.exit_ = QPushButton('关闭', self.win)
-        self.exit_.move(300, 300)
-        self.exit_.show()
-        return self.exit_
+# 设置
+fun = function.function()
+win.setWindowTitle('system of sxy')
+win.setWindowIcon(QIcon("D:\\pythonStudy\\python\\system\\Assets\\图标.jpeg"))
+QToolTip.setFont(QFont('隶书', 15))
+win.setToolTip('sxy system')
+win.setGeometry(300, 230, 400, 340)
+exit_ = QPushButton('关闭', win)
+exit_.move(300, 300)
+exit_.show()
+exit_.clicked.connect(fun.exit_1())
 
-    def show_(self):
-        def job():
-            time.sleep(100)
-
-        _thread.start_new_thread(job, ())
-        _thread.start_new_thread(job, ())
-        job()
-        self.win.show()
-        sys.exit(self.app.exec_())
+# 显示
+win.show()
+sys.exit(app.exec_())
--- system/function.py
+++ system/function.py
@@ -1,18 +1,7 @@
-import UImain
+from PyQt5.QtWidgets import QMessageBox
 
 
-def sitting_exit_function():
-    exit_Button = me.sitting_exitButton()
-
-
-def exit_function():
-    pass
-
-
-def ok():
-    me.show_()
-
-
-me = UImain.UI()
-me.sitting()
-me.show_()
+class function():
+    def exit_1(self):
+        exit_q = QMessageBox()
+        print('aa')
