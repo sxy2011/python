46a28769a8cb854b3f6753f734a4eee41b33e612
Shen x. y <87964621+sxy2011@users.noreply.github.com>
2022-11-06 14:27:18 +0800

    06

--- system/UImain.py
+++ system/UImain.py
@@ -1,43 +1,58 @@
 import sys
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QLabel, QVBoxLayout, QHBoxLayout, QGridLayout, QLineEdit
 
 
 class UI_class(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+	def __init__(self):
+		super().__init__()
+		self.initUI()
+	
+	def initUI(self):
+		dk = app.desktop()
+		QWidget.raise_(self)
+		self.setWindowTitle('system of sxy')
+		self.setWindowIcon(QIcon("D:\\pythonStudy\\python\\system\\Assets\\图标.jpeg"))
+		self.resize(600, 400)
+		self.move(dk.width() / 2 - self.width() / 2, dk.height() / 2 - self.height() / 2)
+		self.createButton_absoluteness('关闭', 525, 375, 70, 20, self.close())
+		self.createLabel_absoluteness('登录', 30, 50, 50, 50)
+		self.show()
+	
+	def createButton_absoluteness(self, text=str, x=int, y=int, width=int, height=int, con=any):
+		self.btn = QPushButton(text, self)
+		self.btn.show()
+		self.btn.resize(width, height)
+		self.btn.move(x, y)
+		self.btn.clicked.connect(con)
+	
+	def createLabel_absoluteness(self, text=str, x=int, y=int, width=int, height=int):
+		self.label = QLabel(text, self)
+		self.label.resize(width, height)
+		self.label.move(x, y)
+		self.label.show()
+	
+	def createLineEdit_absoluteness(self, x=int, y=int, width=int, height=int):
+		code = QLineEdit(self)
+		code.resize(width, height)
+		code.move(x, y)
+		code.show()
+	
+	def createButton_relatively(self, text=str, x=int, y=int, width=int, height=int, con=any):
+		self.btn = QHBoxLayout(self)
+		self.btn.addWidget()
+		self.btn.show()
+		self.btn.resize(width, height)
+		self.btn.move(x, y)
+		self.btn.clicked.connect(con)
+	
+	def closeEvent(self, QCloseEvent):
+		self.exit_q = QMessageBox.question(self, '?', '你确定要关闭吗?', QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+		if self.exit_q == QMessageBox.Yes:
+			QCloseEvent.accept()
+		else:
+			QCloseEvent.ignore()
 
-    def initUI(self):
-        dk = app.desktop()
-        QWidget.raise_(self)
-        self.setWindowTitle('system of sxy')
-        self.setWindowIcon(QIcon("D:\\pythonStudy\\python\\system\\Assets\\图标.jpeg"))
-        self.resize(600, 400)
-        self.move(dk.width() / 2 - self.width() / 2, dk.height() / 2 - self.height() / 2)
-        self.createButton_absoluteness('关闭', 525, 375, 70, 20, self.close)
-        self.createLabel_absoluteness('登录', 30, 50, 1, 1)
-        self.show()
-
-    def createButton_absoluteness(self, text=str, x=int, y=int, width=int, height=int, con=any):
-        self.btn = QPushButton(text, self)
-        self.btn.show()
-        self.btn.resize(width, height)
-        self.btn.move(x, y)
-        self.btn.clicked.connect(con)
-
-    def createLabel_absoluteness(self, text=str, x=int, y=int, width=int, height=int):
-        self.label = QLabel(text, self)
-        self.label.resize(width, height)
-        self.label.move(x, y)
-        self.label.show()
-
-    def closeEvent(self, QCloseEvent):
-        self.exit_q = QMessageBox.question(self, '?', '你确定要关闭吗?', QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
-        if self.exit_q == QMessageBox.Yes:
-            QCloseEvent.accept()
-        else:
-            QCloseEvent.ignore()
 
 app = QApplication(sys.argv)
 win = UI_class()
--- system/command.py
+++ system/command.py
@@ -4,114 +4,115 @@ import save
 
 
 class command:
-    def __init__(self):
-        global systemName, systemKey, nowstate, systemUser, saving, set  # 将所有变量变成全局的
-        saving = save.save()  # 建立保存对象
-        systemName = input('systemName:')  # 询问系统用户名
-        if systemName == '':  # 确定不为空,否则设为默认
-            systemName = 'Administrator'
+	def __init__(self):
+		global systemName, systemKey, nowstate, systemUser, saving, set  # 将所有变量变成全局的
+		saving = save.save()  # 建立保存对象
+		systemName = input('systemName:')  # 询问系统用户名
+		if systemName == '':  # 确定不为空,否则设为默认
+			systemName = 'Administrator'
+		
+		systemKey = input('systemKey:')  # 询问系统密码
+		if systemKey == '':  # 确定不为空,否则设为默认
+			systemKey = '111'
+		
+		systemUser = {  # 建立用户列表
+			systemName: systemKey
+		}
+		nowstate = systemName  # 建立目前状态
+		
+		set = {  # 建立设置状态
+			'autoSave': saving.autoSave()
+		}
+	
+	def get_input(self):  # 主要函数
+		global nowstate
+		while True:  # 建立主循环,判断命令
+			command = input('command ' + nowstate + ' ')  # 接受命令
+			if command == 'exit' and not 'sitting' in nowstate:  # 判断退出
+				print("bye!")
+				sys.exit()
+			
+			elif command == 'add user':  # 判断增加用户
+				if nowstate == systemName:  # 只有管理员才能增加用户
+					userName = input('userName: ')  # 询问用户名
+					userKey = input('userKey: ')  # 询问用户密码
+					if userName in systemUser:  # 判断是否重复
+						print('Existing users')
+					else:
+						# 检测成功,开始设置
+						systemUser[userName] = userKey
+						print('ok')
+				else:
+					print('you are not Administrator')
+			
+			elif command == 'del user':
+				if nowstate == systemName:
+					print(systemUser.keys())
+					delUserName = input("delUserName: ")
+					verification = input('SystemKey: ')
+					if verification == systemKey:
+						delKey = systemUser[delUserName]
+						delName = delUserName
+						try:
+							listUser = list(systemUser.keys())
+							for i in listUser:
+								if i == delName:
+									if i == listUser[0]:
+										print("you can't del the system user")
+										raise IndexError
+							del systemUser[delUserName]
+							print('ok')
+						except:
+							print('error')
+					else:
+						print('authentication failed')
+				else:
+					print('you are not Administrator')
+			
+			
+			elif command == 'switch user':
+				targetUser = input('target user: ')
+				targetUserKey = input('target userKey: ')
+				if targetUser in systemUser:
+					if systemUser[targetUser] == targetUserKey:
+						nowstate = targetUser
+						print('ok')
+					else:
+						print('authentication failed')
+				else:
+					print("I can't find this user")
+			
+			
+			elif command == 'sitting':
+				nowstate = 'sitting' + nowstate
+				print(set)
+				targetSitting = input('ChooseSitting: ')
+				targetValue = input('Value(True / False): ')
+				if (targetValue != True or targetValue != False) and (targetSitting in set.keys()):
+					set[targetSitting] = targetValue
+					print('ok')
+				else:
+					print('error')
+			
+			
+			elif command == 'help':
+				print(
+					'exit : 退出\n'
+					'add user : 增加用户\n'
+					'del user : 删除用户\n'
+					'switch user : 切换用户\n'
+					'sitting : 设置'
+					'UI : 打开界面'
+				)
+			
+			elif command == 'UI':
+				import UImain
+			
+			elif command == 'save':
+				saving.Saving()
+			else:
+				print("I don't know this")
 
-        systemKey = input('systemKey:')  # 询问系统密码
-        if systemKey == '':  # 确定不为空,否则设为默认
-            systemKey = '111'
-
-        systemUser = {  # 建立用户列表
-            systemName: systemKey
-        }
-        nowstate = systemName  # 建立目前状态
-
-        set = {  # 建立设置状态
-            'autoSave': saving.autoSave()
-        }
-
-    def get_input(self):  # 主要函数
-        global nowstate
-        while True:  # 建立主循环,判断命令
-            command = input('command ' + nowstate + ' ')  # 接受命令
-            if command == 'exit' and not 'sitting' in nowstate:  # 判断退出
-                print("bye!")
-                sys.exit()
-
-            elif command == 'add user':  # 判断增加用户
-                if nowstate == systemName:  # 只有管理员才能增加用户
-                    userName = input('userName: ')  # 询问用户名
-                    userKey = input('userKey: ')  # 询问用户密码
-                    if userName in systemUser:  # 判断是否重复
-                        print('Existing users')
-                    else:
-                        # 检测成功,开始设置
-                        systemUser[userName] = userKey
-                        print('ok')
-                else:
-                    print('you are not Administrator')
-
-            elif command == 'del user':
-                if nowstate == systemName:
-                    print(systemUser.keys())
-                    delUserName = input("delUserName: ")
-                    verification = input('SystemKey: ')
-                    if verification == systemKey:
-                        delKey = systemUser[delUserName]
-                        delName = delUserName
-                        try:
-                            listUser = list(systemUser.keys())
-                            for i in listUser:
-                                if i == delName:
-                                    if i == listUser[0]:
-                                        print("you can't del the system user")
-                                        raise IndexError
-                            del systemUser[delUserName]
-                            print('ok')
-                        except:
-                            print('error')
-                    else:
-                        print('authentication failed')
-                else:
-                    print('you are not Administrator')
-
-
-            elif command == 'switch user':
-                targetUser = input('target user: ')
-                targetUserKey = input('target userKey: ')
-                if targetUser in systemUser:
-                    if systemUser[targetUser] == targetUserKey:
-                        nowstate = targetUser
-                        print('ok')
-                    else:
-                        print('authentication failed')
-                else:
-                    print("I can't find this user")
-
-
-            elif command == 'sitting':
-                nowstate = 'sitting' + nowstate
-                print(set)
-                targetSitting = input('ChooseSitting: ')
-                targetValue = input('Value(True / False): ')
-                if (targetValue != True or targetValue != False) and (targetSitting in set.keys()):
-                    set[targetSitting] = targetValue
-                    print('ok')
-                else:
-                    print('error')
-
-
-            elif command == 'help':
-                print(
-                    'exit : 退出\n'
-                    'add user : 增加用户\n'
-                    'del user : 删除用户\n'
-                    'switch user : 切换用户\n'
-                    'sitting : 设置'
-                    'UI : 打开界面'
-                )
-
-            elif command == 'UI':
-                import UImain
-
-            elif command == 'save':
-                saving.Saving()
-            else:
-                print("I don't know this")
 
 def systemUser(self):
-    return systemUser
+	return systemUser
