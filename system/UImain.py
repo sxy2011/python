4a9937a15b95031a3c2dc0e63de4797baecf0f31
Shen x. y <87964621+sxy2011@users.noreply.github.com>
2022-11-06 13:10:50 +0800

    06

--- system/UImain.py
+++ system/UImain.py
@@ -1,32 +1,40 @@
 import sys
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QLabel
 
 
-# noinspection PyTypeChecker
 class UI_class(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-
+    
     def initUI(self):
+        dk = app.desktop()
         self.setWindowTitle('system of sxy')
         self.setWindowIcon(QIcon("D:\\pythonStudy\\python\\system\\Assets\\图标.jpeg"))
         self.resize(600, 400)
-        QToolTip.setFont(QFont('隶书', 15))
-        self.setToolTip('sxy system')
+        self.exitButton()
+        self.loginLabel()
+    
+    def exitButton(self):
         self.exit_ = QPushButton('关闭', self)
         self.exit_.show()
         self.exit_.move(525, 375)
         self.show()
         self.exit_.clicked.connect(self.close)
-
+    
     def closeEvent(self, QCloseEvent):
         self.exit_q = QMessageBox.question(self, '?', '你确定要关闭吗?', QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
         if self.exit_q == QMessageBox.Yes:
             QCloseEvent.accept()
         else:
             QCloseEvent.ignore()
+    
+    def loginLabel(self):
+        self.login_l = QLabel('登录', self)
+        self.login_l.resize(30, 50)
+        self.login_l.move(1, 1)
+        self.login_l.show()
 
 app = QApplication(sys.argv)
 win = UI_class()
