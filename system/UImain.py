d781cad2ec00680fec5347ec8e5ed29b416c4cfd
Shen x. y <87964621+sxy2011@users.noreply.github.com>
2022-11-06 14:00:59 +0800

    06

--- system/UImain.py
+++ system/UImain.py
@@ -1,40 +1,43 @@
 import sys
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QLabel, QVBoxLayout, QHBoxLayout
 
 
 class UI_class(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-    
+
     def initUI(self):
         dk = app.desktop()
+        QWidget.raise_(self)
         self.setWindowTitle('system of sxy')
         self.setWindowIcon(QIcon("D:\\pythonStudy\\python\\system\\Assets\\图标.jpeg"))
         self.resize(600, 400)
-        self.exitButton()
-        self.loginLabel()
-    
-    def exitButton(self):
-        self.exit_ = QPushButton('关闭', self)
-        self.exit_.show()
-        self.exit_.move(525, 375)
+        self.move(dk.width() / 2 - self.width() / 2, dk.height() / 2 - self.height() / 2)
+        self.createButton_absoluteness('关闭', 525, 375, 70, 20, self.close)
+        self.createLabel_absoluteness('登录', 30, 50, 1, 1)
         self.show()
-        self.exit_.clicked.connect(self.close)
-    
+
+    def createButton_absoluteness(self, text=str, x=int, y=int, width=int, height=int, con=any):
+        self.btn = QPushButton(text, self)
+        self.btn.show()
+        self.btn.resize(width, height)
+        self.btn.move(x, y)
+        self.btn.clicked.connect(con)
+
+    def createLabel_absoluteness(self, text=str, x=int, y=int, width=int, height=int):
+        self.label = QLabel(text, self)
+        self.label.resize(width, height)
+        self.label.move(x, y)
+        self.label.show()
+
     def closeEvent(self, QCloseEvent):
         self.exit_q = QMessageBox.question(self, '?', '你确定要关闭吗?', QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
         if self.exit_q == QMessageBox.Yes:
             QCloseEvent.accept()
         else:
             QCloseEvent.ignore()
-    
-    def loginLabel(self):
-        self.login_l = QLabel('登录', self)
-        self.login_l.resize(30, 50)
-        self.login_l.move(1, 1)
-        self.login_l.show()
 
 app = QApplication(sys.argv)
 win = UI_class()
