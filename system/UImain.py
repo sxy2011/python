20f4194b3d298c395a3e573a63c406df2d454466
Shen x. y <87964621+sxy2011@users.noreply.github.com>
2022-11-12 12:37:15 +0800

    06

--- system/UImain.py
+++ system/UImain.py
@@ -41,7 +41,7 @@ class UI_class(QWidget):
 	def createButton_relatively(self, text=str, x=int, y=int, width=int, height=int, con=any):
 		self.btn_control = QHBoxLayout(self)
 		self.btn = QPushButton
-		self.btn_control.addWidget()
+		self.btn_control.addWidget(self)
 		self.btn.show()
 		self.btn.resize(width, height)
 		self.btn.move(x, y)
